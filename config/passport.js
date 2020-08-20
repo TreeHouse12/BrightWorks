6e5251af9eaae7e7f33bfb94fe5f68374ff4226c
Jeff Laubacker <jefflaubacker@Jeffs-MBP.attlocal.net>
2020-08-20 18:16:25 -0400

    Flash

--- config/passport.js
+++ config/passport.js
@@ -17,21 +17,21 @@ passport.use('local.signup', new LocalStrategy({
     passwordField: 'password',
     passReqToCallback: true
 }, function(req, email, password, done) {
-    User.findOne({'email': email}, function(err, user) {
-      if (err) {
-        return done(err);
-      }
-      if (user) {
-        return done(null, false, {message: 'Email is already in use.'});
-      }
-      var newUser = new User();
-      newUser.email = email;
-      newUser.password = newUser.encryptPassword(password);
-      newUser.save(function(err, result) {
-         if (err) {
-          return done(err);
-         }
-         return done(null, newUser);
-      })
+    User.findOne({'email': email}, function (err, user) {
+        if (err) {
+            return done(err);
+        }
+        if (user) {
+            return done(null, false, {message: 'Email already in use.'});
+        }
+        var newUser = new User();
+        newUser.email = email;
+        newUser.password = newUser.encryptPassword(password);
+        newUser.save(function(err, result) {
+          if (err) {
+              return done(err);
+          }
+          return done(null, newUser);
+        });
     });
 }));
