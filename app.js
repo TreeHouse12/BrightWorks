c2e5d3219dc309b9475a5f5093f85c6e0a0f69e1
Jeff Laubacker <jefflaubacker@Jeffs-MBP.attlocal.net>
2020-08-21 18:51:29 -0400

    session

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(function(req, res, next) {
   res.locals.login = req.isAuthenticated();
+  res.locals.session = req.session;
   next();
 });
 
