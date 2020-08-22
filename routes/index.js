95386e14085c387cd46111a427711058344a5aaf
Jeff Laubacker <jefflaubacker@Jeffs-MBP.attlocal.net>
2020-08-22 15:06:25 -0400

    Remove dot

--- routes/index.js
+++ routes/index.js
@@ -38,7 +38,7 @@ router.get('/shopping-cart', function(req, res, next) {
   res.render('shop/shopping-cart', {services: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
-router.get.('/checkout', function(req, res, next) {
+router.get('/checkout', function(req, res, next) {
   if (!req.session.cart) {
     return res.redirect('/shopping-cart');
   }
