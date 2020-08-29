17f32f8a6f08c4d6b4d7bcf54f8d495284d6a3ff
Jeff Laubacker <jefflaubacker@Jeffs-MBP.attlocal.net>
2020-08-29 19:53:19 -0400

    Update

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,19 @@ const Cart = require('../models/cart');
 
 //GET HOME PAGE
 router.get('/', function (req, res, next) {
+  var successMsg = req.flash('success')[0];
   Service.find(function(err, docs) {
     var serviceChunks = [];
     var chunkSize = 3;
     for (var i = 0; i < docs.length; i += chunkSize) {
       serviceChunks.push(docs.slice(i, i + chunkSize));
     }
-    res.render('shop/pricing', { title: 'Shopping Cart', services: serviceChunks });
+    res.render('shop/index', { title: 'Shopping Cart', services: serviceChunks, successMsg: successMsg, noMessages: !successMsg});
   });
 });
 
 router.get('/add-to-chart/:id', function(req, res, next) {
-  var ProductId = req.parms.id;
+  var serviceId = req.parms.id;
   var cart = new Cart(req.session.cart ? req.session.cart : {});
 
   Service.findById(serviceId, function(err, service) {
@@ -38,12 +39,44 @@ router.get('/shopping-cart', function(req, res, next) {
   res.render('shop/shopping-cart', {services: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
-router.get('/checkout', function(req, res, next) {
+router.get('/checkout', isLoggedIn, function(req, res, next) {
   if (!req.session.cart) {
     return res.redirect('/shopping-cart');
   }
   var cart = new Cart(req.session.cart);
-  res.render('shop/checkout', {total: cart.totalPrice});
+  var errMsg = req.flash('error')[0];
+  res.render('shop/checkout', {total: cart.totalPrice, errMsg: errMsg, noError: !errMsg});
+});
+
+router.post('/checkout', isLoggedIn, function(req, res, next) {
+    if (!req.session.cart) {
+        return res.redirect('/shopping-cart');
+    }
+    var cart = new Cart(req.session.cart);
+
+    var stripe = require("stripe")(
+        "sk_test_51HJ5XEKBSotCa0clj3jVJ9eXTXu5zwB4s8pmgpPulMpoIIpMVza85TaapixRyHGHPcJ5fusvpylxRzlNUFEqVwNE00CGBiGbYk"
+    );
+
+    stripe.charges.create({
+        amount: cart.totalPrice * 100,
+        currency: "usd",
+        source: "tok_mastercard", // obtained with Stripe.js
+        description: "Test Charge"
+    }, function(err, charge) {
+        if (err) {
+            req.flash('error', err.message);
+            return res.redirect('/checkout');
+        }
+    });
 });
 
 module.exports = router;
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    req.session.oldUrl = req.url;
+    res.redirect('/user/signin');
+}
