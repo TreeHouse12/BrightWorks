9ee2f1fcdc89d69c156b2b52a9c6d8d5d48aad55
Jeff Laubacker <jefflaubacker@Jeffs-MBP.attlocal.net>
2020-08-30 02:16:22 -0400

    Add to database

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Service = require('../models/service');
 const Cart = require('../models/cart');
+const Order = require('../models/order');
 
 //GET HOME PAGE
 router.get('/', function (req, res, next) {
@@ -68,9 +69,18 @@ router.post('/checkout', isLoggedIn, function(req, res, next) {
             req.flash('error', err.message);
             return res.redirect('/checkout');
         }
-        req.flash('success', 'Successfully bought product!');
-        req.session.cart = null;
-        res.redirect('/');
+        var order = new Order({
+          user: req.user,
+          cart: cart,
+          address: req.body.address,
+          name: req.body.name,
+          paymentId: charge.id
+        });
+        order.save(function(err,result) {
+            req.flash('success', 'Successfully bought product!');
+            req.session.cart = null;
+            res.redirect('/');
+        });
     });
 });
 
