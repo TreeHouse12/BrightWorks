0c6f4f5c52dbe1a0e36be419711071ed4d187250
Jeff Laubacker <jefflaubacker@Jeffs-MBP.attlocal.net>
2020-09-01 21:07:08 -0400

    Test

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const csrf = require('csurf');
 const passport = require('passport');
+const nodemailer = require('nodemailer');
+require('dotenv/config');
 
 const Order = require('../models/order')
 const Cart = require('../models/cart')
@@ -40,6 +42,40 @@ router.post('/send', function (req, res, next) {
     <h3>Message</h3>
     <p>${req.body.message}</p>
   `;
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+  host: 'mail.brightworksmaintenance.com',
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+      user: process.env.EMAIL_USER, // generated ethereal user
+      pass: process.env.EMAIL_PASSWORD  // generated ethereal password
+  },
+  tls:{
+    rejectUnauthorized:false
+  }
+});
+
+// setup email data with unicode symbols
+let mailOptions = {
+    from: '"Contact" <>', // sender address
+    to: 'support@brightworksmaintenance.com', // list of receivers
+    subject: 'Node Contact Request', // Subject line
+    text: 'Hello world?', // plain text body
+    html: output // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+    res.render('user/contact', {msg:'Email has been sent'});
+});
 });
 
 router.get('/logout', isLoggedIn, function (req, res, next) {
